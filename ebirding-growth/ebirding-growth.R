4fc064a558ec0a048e4be5b678c7d4f038b4e4b9
birdcountindia <skimmer@birdcount.in>
2024-02-20 10:53:57 +0530

    updated ebirding-growth with relJan-2024

--- ebirding-growth/ebirding-growth.R
+++ ebirding-growth/ebirding-growth.R
@@ -1,47 +1,72 @@
 require(tidyverse)
 require(glue)
 
-
 cur_unit <- "India"
 
-# summarising number of lists per year
 
+# summarising number of lists per year
 data_growth <- data %>% 
+  # complete lists
+  filter(ALL.SPECIES.REPORTED == 1) %>% 
   # if state, filter for state
   group_by(YEAR) %>% 
-  summarise(TOT.LISTS = n_distinct(SAMPLING.EVENT.IDENTIFIER))
+  reframe(TOT.LISTS = n_distinct(SAMPLING.EVENT.IDENTIFIER))
 
-write.csv("ebirding-growth/tables/.csv") ###
+write_csv(data_growth,
+          glue("{path_growth}growth_rel{currel_month_lab}-{currel_year}_IN.csv"))
+
 
 # breaks for y axis (different for states)
 y_breaks <- seq(0, max(data_growth$TOT.LISTS), length.out = 5) %>% 
   round(digits = -2)
 
-y_breaks_lab <- glue("{y_breaks} K")
+y_breaks_lab <- glue("{round(y_breaks/1000, 1)} K")
+
+y_t1 <- data_growth %>% 
+  filter(YEAR == 2014) %>% 
+  pull(TOT.LISTS)
+
+x_breaks <- 2000:(currel_year - 1)
+
 
 # plotting the line graph
 
-dataprog_lists <- ggplot(data_growth, aes(x = YEAR, y = TOT.LISTS/1000)) +
+dataprog_lists <- ggplot(data_growth, aes(x = YEAR, y = TOT.LISTS)) +
   geom_point(size = 3, colour = "#fcfa53") +
-  geom_line(size = 1.5, colour = "#fcfa53") +
-  geom_hline(yintercept = 2, linetype = "dotted") +
-  scale_x_continuous(breaks = 2014:rel_year,
+  geom_line(linewidth = 1, colour = "#fcfa53") +
+  geom_hline(yintercept = y_t1, linetype = "dotted", colour = "#CCCCCC80") +
+  geom_text(aes(x = 2014, y = y_t1, 
+                label = glue("{round(y_t1/1000, 1)} K")),
+            size = 3, colour = "#CCCCCC", nudge_y = -15000) +
+  scale_x_continuous(breaks = x_breaks,
                      # interested in after eBird established in India
-                     limits = c(2014, rel_year)) +
+                     limits = c(min(x_breaks), max(x_breaks))) +
   scale_y_continuous(breaks = y_breaks,
                      labels = y_breaks_lab) +
+  coord_cartesian(clip = "off") +
   labs(x = "Year", y = "Number of lists (in thousands)",
        title = glue("Growth of eBird in {cur_unit}"),
-       subtitle = glue("Data until {rel_month_lab} {rel_year}")) +
-  theme(axis.text.x = element_text(size = 8),
-        axis.text = element_text(colour = "#E0E0E0"),
-        axis.title = element_text(colour = "#CCCCCC"),
+       subtitle = glue("Data until {currel_month_lab} {currel_year}")) +
+  theme_void() +
+  theme(axis.text.x = element_text(colour = "#CCCCCC", size = 8),
+        axis.text.y = element_text(colour = "#CCCCCC", size = 9, hjust = 1,
+                                   margin = margin(0, 10, 0, 0)),
+        axis.title.y = element_text(angle = 90, vjust = 6, colour = "#CCCCCC"),
+        axis.title.x = element_text(colour = "#CCCCCC", vjust = -2),
+        
         axis.ticks = element_blank(),
-        plot.title = element_text(size = 20, face = "bold", colour = "#CCCCCC", hjust = -0.08),
-        plot.margin = unit(c(1.5, 1, 0.5, 1), "lines"),
+        panel.grid.major.y = element_line(linetype = "longdash", colour = "#AAAAAA50"),
+        
+        plot.title = element_text(size = 20, face = "bold", colour = "#CCCCCC",
+                                  hjust = -0.15, vjust = 5),
+        plot.subtitle = element_text(size = 14, face = "bold", colour = "#AAAAAA50",
+                                     hjust = -0.11, vjust = 5),
+        plot.margin = unit(c(2, 1, 1, 2), "lines"),
+        
         plot.background = element_rect(fill = "black", colour = NA),
         panel.background = element_rect(fill = "black", colour = NA))
  
-###
-ggsave("ebirding-growth/graphs/.png", dataprog_lists,
+
+ggsave(glue("{path_growth}growth_rel{currel_month_lab}-{currel_year}_IN.png"), 
+       dataprog_lists,
        width = 11, height = 7, units = "in", dpi = 300)
--- monthly-param-auto.R
+++ monthly-param-auto.R
@@ -30,21 +30,30 @@ pmpdatapath <- glue("EBD/pmp_rel{currel_month_lab}-{currel_year}.RData")
 mcdatapath <-  glue("EBD/ebd_IN_rel{currel_month_lab}-{currel_year}_{toupper(currel_month_lab)}.RData")
 ycdatapath <-  glue("EBD/ebd_IN_rel{currel_month_lab}-{currel_year}_{currel_year}.RData")
 
+
+# coverage paths
+
 # for country
-coveragedatapath <- glue("ebirding-coverage/data/{currel_year}-{currel_month_num}/rel{currel_month_lab}-{currel_year}_IN.csv")
-coveragemappath1 <- glue("ebirding-coverage/maps/{currel_year}-{currel_month_num}/rel{currel_month_lab}-{currel_year}_IN_annot.png")
-coveragemappath2 <- glue("ebirding-coverage/maps/{currel_year}-{currel_month_num}/rel{currel_month_lab}-{currel_year}_IN_plain.png")
+coveragedatapath <- glue("ebirding-coverage/data/{currel_year}-{str_pad(currel_month_num, width=2, pad='0')}/rel{currel_month_lab}-{currel_year}_IN.csv")
+coveragemappath1 <- glue("ebirding-coverage/maps/{currel_year}-{str_pad(currel_month_num, width=2, pad='0')}/rel{currel_month_lab}-{currel_year}_IN_annot.png")
+coveragemappath2 <- glue("ebirding-coverage/maps/{currel_year}-{str_pad(currel_month_num, width=2, pad='0')}/rel{currel_month_lab}-{currel_year}_IN_plain.png")
 
 # for states
 state_info <- read_csv("ebirding-coverage/state_info.csv", show_col_types = F)
 
 coveragepaths_st <- state_info %>% 
   # paths without file names
-  mutate(PATH.DATA = glue("ebirding-coverage/data/{currel_year}-{currel_month_num}/"),
-         PATH.MAPS = glue("ebirding-coverage/maps/{currel_year}-{currel_month_num}/")) %>% 
+  mutate(PATH.DATA = glue("ebirding-coverage/data/{currel_year}-{str_pad(currel_month_num, width=2, pad='0')}/"),
+         PATH.MAPS = glue("ebirding-coverage/maps/{currel_year}-{str_pad(currel_month_num, width=2, pad='0')}/")) %>% 
   mutate(DATA = glue("{PATH.DATA}rel{currel_month_lab}-{currel_year}_IN-{STATE.CODE}.csv"),
          MAP1 = glue("{PATH.MAPS}rel{currel_month_lab}-{currel_year}_IN-{STATE.CODE}_annot.png"),
          MAP2 = glue("{PATH.MAPS}rel{currel_month_lab}-{currel_year}_IN-{STATE.CODE}_plain.png"))
+
+
+# growth paths
+
+path_growth <- glue("ebirding-growth/{currel_year}-{str_pad(currel_month_num, width=2, pad='0')}/")
+
 
 # creating folders if they don't exist
 if (!dir.exists(coveragepaths_st$PATH.DATA[1])) {
@@ -53,4 +62,8 @@ if (!dir.exists(coveragepaths_st$PATH.DATA[1])) {
 
 if (!dir.exists(coveragepaths_st$PATH.MAPS[1])) {
   dir.create(coveragepaths_st$PATH.MAPS[1])
+}
+
+if (!dir.exists(path_growth)) {
+  dir.create(path_growth)
 }
